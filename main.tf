251ec4f1a5f397aab4dbccbdbf3ee0d95794cdaa
Simon Lynch <simon.lynch@hashicorp.com>
2023-03-07 11:12:40 +1100

    inputs

--- main.tf
+++ main.tf
@@ -14,20 +14,21 @@ module "workpace" {
 
   workspace_terraform_version = var.workspace_terraform_version
   workspace_tags = var.workspace_tags
+
   variables = var.variables
 
   remote_state = var.remote_state
   remote_state_consumers = var.remote_state_consumers
 
+  #VCS Configuration
   vcs_repo = var.vcs_repo
   workspace_vcs_directory = var.workspace_vcs_directory
   workspace_auto_apply = var.workspace_auto_apply
 
-
+  #Agents
   workspace_agents = var.workspace_agents
   execution_mode = var.execution_mode
   agent_pool_name = var.agent_pool_name
-
   assessments_enabled = var.assessments_enabled
 
   workspace_read_access_emails = var.workspace_read_access_emails
