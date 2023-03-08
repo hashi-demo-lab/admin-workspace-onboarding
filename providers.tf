ec5dac2fa9cc00bfdf8bbfd38de4e974c032c09d
Simon Lynch <simon.lynch@hashicorp.com>
2023-03-08 18:09:34 +1100

    functional

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+## Place your Terraform Args / Provider version args here
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "5.18.0"
+    }
+  }
+}
 
 provider "github" {
   # Configuration options
